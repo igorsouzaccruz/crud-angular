ff5d640e21fca1c0741f7a8e150149fd2ebf7f93
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-04 11:36:14 -0300

    table not working and add shared

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+
+const courses:Course[] = [
+  {_id: '1', name:'Angular', category:'front-end'},
+  {_id: '2', name:'Teste ', category:'front-end'},
+];
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent {
+
+  displayedColumns:string[] = ['name','category'];
+  courses = courses;
 
 }
