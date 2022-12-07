2d3393930879800eeb294f4a155a7f78f33b6d97
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-07 17:20:13 -0300

    spinner

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -11,14 +11,14 @@ import { CoursesService } from '../services/courses.service';
 
 export class CoursesComponent implements OnInit{
 
-  courses:Observable<Course[]>;
+  courses$:Observable<Course[]>;
   displayedColumns = ['name','category'];
 
   // coursesService:CoursesService;
 
   constructor(private coursesService:CoursesService){
     // this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
   ngOnInit(): void {
     // this.courses = this.coursesService.list();
