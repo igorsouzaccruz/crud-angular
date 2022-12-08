fe3239239fec087b272af259a10789a5ccd7b3df
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-08 14:49:24 -0300

    conectando com spring e add id column

---
+++ proxy.conf.js
@@ -0,0 +1,10 @@
+const PROXT_CONFIG = [
+    {
+        context: ['/api'],
+        target: 'http://localhost:8080/',
+        secure: false,
+        logLevel: 'debug'
+    }
+];
+
+module.exports = PROXT_CONFIG;
